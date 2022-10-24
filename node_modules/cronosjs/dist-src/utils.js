82ffe3ada99ba97874e2d872103440a080f1c539
root <softwaredev@Bitpool.com>
2022-10-24 01:55:36 +0000

    added gitignore

--- node_modules/cronosjs/dist-src/utils.js
+++
@@ -1,7 +0,0 @@
-export const sortAsc = (a, b) => a - b;
-export function flatMap(arr, mapper) {
-    return arr.reduce((acc, val, i) => {
-        acc.push(...mapper(val, i, arr));
-        return acc;
-    }, []);
-}
--- node_modules/cronosjs/dist-types/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const sortAsc: (a: number, b: number) => number;
-export declare function flatMap<T, U>(arr: T[], mapper: (value: T, index: number, array: T[]) => U[]): U[];
